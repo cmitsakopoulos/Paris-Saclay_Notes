1a20c1a373e5b012a44b87ea243a11cea08c5733
Raul Duran De Alba <62449911+rauldd-sys@users.noreply.github.com>
2024-10-09 11:29:14 +0200

    uh

--- POO/List_dunder/my_list.py
+++ POO/List_dunder/my_list.py
@@ -4,11 +4,16 @@ class MyList:
     def __init__(self):
         self.head = None
     
+    
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
-            return
+        else:
+            current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = new_node
 
 
 if __name__=='__main__':
